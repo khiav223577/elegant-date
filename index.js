69d7e86d049eaeabc00df246a48992075cfa74d1
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2018-04-13 18:00:21 +0800

    add ElegantDate

---
+++ index.js
@@ -0,0 +1,13 @@
+function ElegantDate(date){
+  return {
+    get date(){ return date; },
+    get beginningOfMonth(){
+      var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+      return elegantDate(firstDay);
+    },
+    get endOfMonth(){
+      var lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+      return elegantDate(lastDay);
+    },
+  }
+}
